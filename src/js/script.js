f125b60b0c1a49babdfad67a5cd0d1b727919ade
rolandszilagyi87 <bobszinkler@icloud.com>
2025-04-06 09:29:07 +0200

    feat_ham-logic-upd

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamIcon = document.querySelector(".header__ham img");
   const nav = document.querySelector(".js-nav");
+  const main = document.querySelector("main");
 
   hamIcon.addEventListener("click", function() {
     nav.classList.toggle('d-flex');
     nav.classList.toggle("d-none");
+    main.classList.toggle("d-none");
   });
 
   nav.addEventListener("click", function() {
     nav.classList.toggle('d-flex');
     nav.classList.toggle("d-none");
+    main.classList.toggle("d-none");
   });
 });
